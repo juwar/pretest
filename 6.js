736b482fb5262151ae3a79d09a93e18f2d1e3f40
juwar <50343114+juwar@users.noreply.github.com>
2019-06-16 07:56:29 +0700

    Update 6.js

--- 6.js
+++ 6.js
@@ -5,15 +5,13 @@ function convert(nilai){
         if( nilai % 2 == 0){
             hasil += '0'
             nilai = nilai / 2
-            if(nilai == 1){
-                hasil += '1'
-            }
         } else {
             hasil += '1'
-            nilai = (nilai - 1) / 2
-            if(nilai == 1){
-                hasil += '1'
-            }
+            nilai = (nilai - 1) / 2 
+        }
+        
+        if(nilai == 1){
+            hasil += '1'
         }
     }
 
